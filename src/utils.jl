93478e6bd5ec82d57c5a75aac1bc5c5117664744
ayushinav <abhinav.ism.iit@gmail.com>
2024-07-04 09:48:31 -0400

    deeponet multi-output fix

--- src/utils.jl
+++ src/utils.jl
@@ -9,3 +9,38 @@ end
     # FIXME: This is not good for performance but that is okay for now
     return stack(*, eachslice(x; dims=3), eachslice(y; dims=3))
 end
+
+@inline function __project(b::AbstractArray{T1, 2}, t::AbstractArray{T2, 3}) where {T1, T2}
+    # b : p x nb
+    # t : p x N x nb
+    @show size.([b, t])
+    b_ = reshape(b, size(b, 1), 1, size(b, 2)) # p x 1 x nb
+    return dropdims(sum(b_ .*t, dims = 1), dims = 1) # N x nb
+end
+
+@inline function __project(b::AbstractArray{T1, 3}, t::AbstractArray{T2, 3}) where {T1, T2}
+    # b : p x u x nb
+    # t : p x N x nb
+    @show size.([b, t])
+    if size(b, 2) == 1 || size(t, 2) == 1
+        return sum(b .* t, dims = 1) # 1 x N x nb
+    else
+        return LuxNeuralOperators.__batched_mul(batched_adjoint(t), b) # N x p x nb
+    end
+end
+
+@inline function __project(b::AbstractArray{T1, N}, t::AbstractArray{T2, 3}) where {T1, T2, N}
+    # b : p x u_size x nb
+    # t : p x N x nb
+    @show size.([b, t])
+
+    u_size = size(b)[2:end-1]
+
+    b_ = reshape(b, size(b,1), 1, u_size..., size(b)[end])
+    # p x u_size x 1 x nb
+
+    t_ = reshape(t, size(t)[1:2]..., ones(eltype(u_size), length(u_size))..., size(t)[end])
+    # p x (1,1,1...) X N x nb
+
+    return dropdims(sum(b_ .* t_; dims = 1), dims = 1) # u_size x N x nb
+end
--- test/deeponet_tests.jl
+++ test/deeponet_tests.jl
@@ -2,49 +2,55 @@
     @testset "BACKEND: $(mode)" for (mode, aType, dev, ongpu) in MODES
         rng = StableRNG(12345)
 
-        u = rand(Float32, 64, 5) |> aType # sensor_points x nb
-        y = rand(Float32, 1, 10, 5) |> aType # ndims x N x nb
-        out_size = (10, 5)
+        setups = [
+            (u_size=(64, 5), y_size=(1, 10, 5), out_size=(10, 5),
+                branch=(64, 32, 32, 16), trunk=(1, 8, 8, 16), name="Scalar"),
+            (u_size=(64, 3, 5), y_size=(4, 10, 5), out_size=(10, 3, 5),
+                branch=(64, 32, 32, 16), trunk=(1, 8, 8, 16), name="Vector"),
+            (u_size=(64, 4, 3, 3, 5), y_size=(4, 10, 5), out_size=(10, 4, 3, 3, 5),
+                branch=(64, 32, 32, 16), trunk=(1, 8, 8, 16), name="Tensor")]
 
-        deeponet = DeepONet(; branch=(64, 32, 32, 16), trunk=(1, 8, 8, 16))
-        display(deeponet)
-        ps, st = Lux.setup(rng, deeponet) |> dev
-
-        @inferred deeponet((u, y), ps, st)
-        @jet deeponet((u, y), ps, st)
-
-        pred = first(deeponet((u, y), ps, st))
-        @test size(pred) == out_size
-
-        deeponet = DeepONet(Chain(Dense(64 => 32), Dense(32 => 32), Dense(32 => 16)),
-            Chain(Dense(1 => 8), Dense(8 => 8), Dense(8 => 16)))
-        display(deeponet)
-        ps, st = Lux.setup(rng, deeponet) |> dev
-
-        @inferred deeponet((u, y), ps, st)
-        @jet deeponet((u, y), ps, st)
-
-        pred = first(deeponet((u, y), ps, st))
-        @test size(pred) == out_size
-
-        deeponet = DeepONet(Chain(Dense(64 => 32), Dense(32 => 32), Dense(32 => 20)),
-            Chain(Dense(1 => 8), Dense(8 => 8), Dense(8 => 16)))
-        display(deeponet)
-        ps, st = Lux.setup(rng, deeponet) |> dev
-        @test_throws ArgumentError deeponet((u, y), ps, st)
-
-        @testset "higher-dim input #7" begin
-            u = ones(Float32, 10, 10, 10) |> aType
-            v = ones(Float32, 1, 10, 10) |> aType
-            deeponet = DeepONet(; branch=(10, 10, 10), trunk=(1, 10, 10))
+        @testset "$(setup.name)" for setup in setups
+            u = rand(Float32, setup.u_size...) |> aType
+            y = rand(Float32, setup.y_size...) |> aType
+            deeponet = DeepONet(; branch=setup.branch, trunk=setup.trunk)
             display(deeponet)
             ps, st = Lux.setup(rng, deeponet) |> dev
 
-            y, st_ = deeponet((u, v), ps, st)
-            @test size(y) == (10, 10)
+            @inferred first(deeponet((u, y), ps, st))
+            @jet first(deeponet((u, y), ps, st))
 
-            @inferred deeponet((u, v), ps, st)
-            @jet deeponet((u, v), ps, st)
+            pred = first(deeponet(u, y), ps, st)
+            @test size(setup.out_size) == size(pred)
+        end
+
+        @testset "Additonal layer" begin
+            u = rand(Float32, 64, 1, 5) |> aType # sensor_points x nb
+            y = rand(Float32, 1, 10, 5) |> aType # ndims x N x nb
+            out_size = (4, 10, 5)
+
+            branch_net = Chain(Dense(64 => 32), Dense(32 => 32), Dense(32 => 16))
+            trunk_net = Chain(Dense(1 => 8), Dense(8 => 8), Dense(8 => 16))
+            additional = Chain(Dense(1 => 4))
+            deeponet = DeepONet(branch_net, trunk_net; additional=additional)
+            display(deeponet)
+            ps, st = Lux.setup(rng, deeponet)
+            @inferred deeponet((u, y), ps, st)
+            @jet deeponet((u, y), ps, st)
+
+            pred = first(deeponet((u, y), ps, st))
+            @test size(out_size) == size(pred)
+        end
+
+        @testset "Embedding layer mismatch" begin
+            u = rand(Float32, 64, 5) |> aType
+            y = rand(Float32, 1, 10, 5) |> aType
+
+            deeponet = DeepONet(Chain(Dense(64 => 32), Dense(32 => 32), Dense(32 => 20)),
+                Chain(Dense(1 => 8), Dense(8 => 8), Dense(8 => 16)))
+                display(deeponet)
+            ps, st = Lux.setup(rng, deeponet) |> dev
+            @test_throws ArgumentError deeponet((u, y), ps, st)
         end
     end
 end
