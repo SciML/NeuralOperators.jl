71f267ea45953c97b38c2eed2168aef91e79e5b1
Avik Pal <avikpal@mit.edu>
2024-08-04 17:21:19 -0400

    feat: setup basic flux code
    [skip ci] [skip docs]

--- bench/flux.jl
+++ bench/flux.jl
@@ -4,22 +4,7 @@ threadinfo()
 
 using BenchmarkTools, NeuralOperators, Random, Optimisers, Zygote
 
-rng = Xoshiro(1234)
-
-train!(args...; kwargs...) = train!(MSELoss(), AutoZygote(), args...; kwargs...)
-
-function train!(loss, backend, model, ps, st, data; epochs=10)
-    l1 = loss(model, ps, st, first(data))
-
-    tstate = Lux.Experimental.TrainState(model, ps, st, Adam(0.01f0))
-    for _ in 1:epochs, (x, y) in data
-        _, _, _, tstate = Lux.Experimental.single_train_step!(backend, loss, (x, y), tstate)
-    end
-
-    l2 = loss(model, ps, st, first(data))
-
-    return l2, l1
-end
+# TODO: Add training code
 
 # FNO
 n_points = 128
@@ -30,18 +15,18 @@ y = rand(Float32, 1, n_points, batch_size);
 data = [(x, y)];
 t_fwd = zeros(5)
 t_train = zeros(5)
+
 for i in 1:5
     chs = (1, 128, fill(64, i)..., 128, 1)
-    model = FourierNeuralOperator(gelu; chs=chs, modes=(16,))
-    ps, st = Lux.setup(rng, model)
-    model(x, ps, st) # TTFX
+    model = FourierNeuralOperator(; ch=chs, modes=(16,), σ=gelu)
+    # model(x) # TTFX
 
-    t_fwd[i] = @belapsed $model($x, $ps, $st)
+    # t_fwd[i] = @belapsed $model($x)
 
-    t_train[i] = @belapsed train!($model, $ps, $st, $data; epochs=10)
+    # t_train[i] = @belapsed train!($model, $ps, $st, $data; epochs=10)
 end
 
-println("\n## FNO")
+println("\n## FNO (Flux NeuralOperators.jl)")
 print("| #layers | Forward | Train: 10 epochs | \n")
 print("| --- | --- | --- | \n")
 for i in 1:5
@@ -65,16 +50,15 @@ t_train = zeros(5)
 for i in 1:5
     ch_branch = (m, fill(64, i)..., 128)
     ch_trunk = (dim_y, fill(64, i)..., 128)
-    model = DeepONet(; branch=ch_branch, trunk=ch_trunk)
-    ps, st = Lux.setup(rng, model)
-    model((u, y), ps, st) # TTFX
+    model = DeepONet(ch_branch, ch_trunk)
+    # model(u, y) # TTFX
 
-    t_fwd[i] = @belapsed $model(($u, $y), $ps, $st)
+    # t_fwd[i] = @belapsed $model($u, $y)
 
-    t_train[i] = @belapsed train!($model, $ps, $st, $data; epochs=10)
+    # t_train[i] = @belapsed train!($model, $ps, $st, $data; epochs=10)
 end
 
-println("\n## DeepONet")
+println("\n## DeepONet (Flux NeuralOperators.jl)")
 print("| #layers | Forward | Train: 10 epochs | \n")
 print("| --- | --- | --- | \n")
 for i in 1:5
